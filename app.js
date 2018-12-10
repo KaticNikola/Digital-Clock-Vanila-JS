e36078293c2c91d008ed7f3d77ebf02fb203ef9b
KaticNikola <35155375+KaticNikola@users.noreply.github.com>
2018-12-10 11:09:22 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const clock = document.getElementById("myClockDispay");
+
+
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM"
+
+    if (h == 0 ) {
+        h = 12;
+    }
+    if (h > 12) {
+         h = h -12;
+        session = "PM";
+    }
+   // // ako ti pokazuje sa jednom cifrom vreme ovo je resneje
+
+   h = (h < 10) ? "0" + h : h;
+   m = (m < 10) ? "0" + m : m;
+   s = (s < 10) ? "0" + s : s;
+
+
+    let time = h + ":" + m + ":" + s + " " + session;
+    clock.innerHTML = time;
+    clock.textContent = time;
+
+    setTimeout(showTime, 1000);
+
+};
+showTime();
+// setInterval(showTime, 1000); //da ucitava stalno
